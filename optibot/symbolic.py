dc3ae9a90284f1ff0f7551ff69ce2fae1d26062a
Siro Moreno <smorenom@pc-iri-5.upc.edu>
2021-06-29 15:08:40 +0200

    better handling of symbolic expressions, generalized detection of dynamic symbols

--- optibot/symbolic.py
+++ optibot/symbolic.py
@@ -23,6 +23,8 @@ from sympy import (
     lambdify,
     sqrt,
     Symbol,
+    Derivative,
+    Function,
 )
 from sympy.physics.mechanics import dynamicsymbols
 from sympy.functions import sign
@@ -31,6 +33,51 @@ from sympy.physics.mechanics import LagrangesMethod
 
 def get_str(x):
     return x.__str__()
+
+
+def derivative_level(symb):
+    dot_level = 0
+    while type(symb) == Derivative:
+        symb = symb.integrate()
+        dot_level += 1
+    return dot_level
+
+
+def deriv_base(symb):
+    while type(symb) == Derivative:
+        symb = symb.integrate()
+    return symb
+
+
+def sorted_dynamic_symbols(expr):
+    """
+    In a given expression, finds dynamic symbols, understanding them as either
+    functions that only depend on t or derivatives of such functions.
+
+    Parameters
+    ----------
+    expr : sympy expression
+        DESCRIPTION.
+
+    Returns
+    -------
+    dyn_vars : list
+        List of found dynamic symbols, ordered from biggest to smallest
+        derivation order
+
+    """
+    t = symbols("t")
+    dyn_vars = []
+    func_set = expr.atoms(Function)
+    deriv_set = expr.atoms(Derivative)
+    for func in func_set:
+        if func.args == (t,):
+            dyn_vars.append(func)
+    for deriv in deriv_set:
+        if deriv_base(deriv).args == (t,):
+            dyn_vars.append(deriv)
+    dyn_vars = sorted(dyn_vars, key=derivative_level, reverse=True)
+    return dyn_vars
 
 
 def integerize(expr):
@@ -101,18 +148,46 @@ def get_lagr_eqs(T, U, n_vars):
     return res
 
 
-def diff_to_symb(expr, n_var):
+def diff_to_symb(symb):
+    q = symb
+    dot_level = 0
+    while type(q) == Derivative:
+        q = q.integrate()
+        dot_level += 1
+    q_name = q.__str__()[:-3]
+    if dot_level == 0:
+        pass
+    elif q_name[:2] == "q_" or (q_name[0] == "q" and q_name[1].isdigit()):
+        if dot_level == 1:
+            q_name = "v" + q_name[1:]
+        elif dot_level == 2:
+            q_name = "a" + q_name[1:]
+        else:
+            q_name = "a" + "_dot" * (dot_level - 2) + q_name[1:]
+    else:
+        q_name += "_dot" * (dot_level)
+
+    return symbols(q_name)
+
+
+def diff_to_symb_expr(expr):
     """Transform an expression with derivatives to symbols"""
-    t = symbols("t")
-    for jj in range(n_var):
-        q = dynamicsymbols(f"q_{jj}")
-        expr = expr.subs(
-            (
-                [q.diff(t, 2), symbols(f"a_{jj}")],
-                [q.diff(t), symbols(f"v_{jj}")],
-                [q, symbols(f"q_{jj}")],
-            )
-        )
+    dyn_vars = sorted_dynamic_symbols(expr)
+    subs_list = [[dvar, diff_to_symb(dvar)] for dvar in dyn_vars]
+    new_expr = expr.subs(subs_list)
+    return new_expr
+
+
+def standard_notation(expr):
+    var_set = expr.atoms(Symbol)
+    n_vars_max = len(var_set)
+    subs_list = []
+    for jj in range(n_vars_max):
+        subs_list.append([symbols(f"q{jj}"), symbols(f"q_{jj}")])
+        subs_list.append([symbols(f"v{jj}"), symbols(f"v_{jj}")])
+        subs_list.append([symbols(f"a{jj}"), symbols(f"a_{jj}")])
+        subs_list.append([symbols(f"u{jj}"), symbols(f"u_{jj}")])
+    expr = expr.subs(subs_list)
     return expr
 
 
@@ -146,7 +221,7 @@ def lagr_to_RHS(lagr_eqs, output_msgs=True):
     if output_msgs:
         print("Calculating matrices")
     for ii in range(n_var):
-        expr = diff_to_symb(lagr_eqs[ii], n_var)
+        expr = diff_to_symb_expr(lagr_eqs[ii])
         coeff_line = []
         rest = expr
         for jj in range(n_var):
@@ -294,7 +369,7 @@ class SimpLagrangesMethod:
         return self.RHS_full
 
 
-def print_funcs_RHS(RHS, n_var, flavour="numpy"):
+def print_funcs_RHS(RHS, q_vars, u_vars=None, flavour="numpy"):
     """
     Prints the Right Hand Side of the control ecuations, formatted
     to be used as a python function to solve a system like:
@@ -304,8 +379,13 @@ def print_funcs_RHS(RHS, n_var, flavour="numpy"):
     ----------
     RHS : Matrix or list of symbolic expressions
         
-    n_var : int
-        Number of variables
+    q_vars : int or list of symbols
+        Number of q variables or list of q variables as symbols.
+        If int, will search variables of form q_i
+        
+    u_vars : None or list of symbols
+        Number of u variables or list of q variables as symbols.
+        If None, will search variables of form u_i
     
     flavour : str in ["numpy", "casadi"], default = "numpy"
         experimental feature, converts common functions like sin(x)
@@ -319,21 +399,44 @@ def print_funcs_RHS(RHS, n_var, flavour="numpy"):
 
     """
     RHS = list(RHS)
-    q_args = []
-    v_args = []
+    RHS = [standard_notation(diff_to_symb_expr(expr)) for expr in RHS]
+    max_n_var = max([len(expr.atoms(Symbol)) for expr in RHS])
     u_args = []
     params = []
     args = []
     funcs = []
-    for jj in range(n_var):
-        q = symbols(f"q_{jj}")
-        q_args.append(q)
-        v = symbols(f"v_{jj}")
-        v_args.append(v)
-        u = symbols(f"u_{jj}")
-        u_args.append(u)
-        args += [q, v, u]
+    u_args_found = []
+    x_args_found = []
+
+    if type(q_vars) == int:
+        q_args = []
+        v_args = []
+        for jj in range(q_vars):
+            q = symbols(f"q_{jj}")
+            q_args.append(q)
+            v = symbols(f"v_{jj}")
+            v_args.append(v)
+    elif type(q_vars) == list:
+        q_args = [diff_to_symb(var) for var in q_vars]
+        v_args = [diff_to_symb(var.diff()) for var in q_vars]
+    else:
+        raise TypeError(
+            "data type not undersood for q_vars, must be an integer or list of symbols"
+        )
+
+    if u_vars is None:
+        for jj in range(max_n_var):
+            u = symbols(f"u_{jj}")
+            u_args.append(u)
+    elif type(u_vars) == list:
+        u_args = u_vars
+    else:
+        raise TypeError(
+            "data type not undersood for u_vars, must be an integer or list of symbols"
+        )
+
     x_args = q_args + v_args
+    args = x_args + u_args
     for ii in range(len(RHS)):
         expr = RHS[ii]
         var_set = expr.atoms(Symbol)
@@ -341,12 +444,22 @@ def print_funcs_RHS(RHS, n_var, flavour="numpy"):
             if not symb in args:
                 if not symb in params:
                     params.append(symb)
+            elif symb in u_args:
+                if not symb in u_args_found:
+                    u_args_found.append(symb)
+            elif symb in x_args:
+                if not symb in x_args_found:
+                    x_args_found.append(symb)
         funcs.append(expr)
 
     params = sorted(params, key=get_str)
+    if u_vars is None:
+        u_args_found = sorted(u_args_found, key=get_str)
+    else:
+        u_args_found = u_vars
     msg = "def F(x, u, params):\n"
     msg += f"    {x_args.__str__()[1:-1]} = unpack(x)\n"
-    msg += f"    {u_args.__str__()[1:-1]} = unpack(u)\n"
+    msg += f"    {u_args_found.__str__()[1:-1]} = unpack(u)\n"
     msg += f"    {params.__str__()[1:-1]} = params\n"
     msg += f"    result = [{v_args.__str__()[1:-1]},]\n"
     for expr in funcs:
@@ -365,8 +478,9 @@ def print_funcs(expr_list, n_var=0, flavour="numpy"):
     ----------
     expr_list : Matrix or list of Sympy symbolic expressions
         
-    n_var : int, default = 0
-        Number of variables q contained in x
+    n_var : int or list of symbols, default = 0
+        Number of q variables or list of q variables as symbols.
+        If int, will search variables of form q_i and qi
         If set to 0, all detected variables will be considered parameters
     
     flavour : str in ["numpy", "casadi"], default = "numpy"
@@ -381,6 +495,8 @@ def print_funcs(expr_list, n_var=0, flavour="numpy"):
 
     """
     expr_list = list(expr_list)
+    expr_list = [standard_notation(diff_to_symb_expr(expr)) for expr in expr_list]
+    max_n_var = max([len(expr.atoms(Symbol)) for expr in expr_list])
     q_args = []
     v_args = []
     u_args = []
@@ -389,15 +505,29 @@ def print_funcs(expr_list, n_var=0, flavour="numpy"):
     params = []
     args = []
     funcs = []
-    for jj in range(n_var):
-        q = symbols(f"q_{jj}")
-        q_args.append(q)
-        v = symbols(f"v_{jj}")
-        v_args.append(v)
+    if type(n_var) == int:
+        q_args = []
+        v_args = []
+        for jj in range(n_var):
+            q = symbols(f"q_{jj}")
+            q_args.append(q)
+            v = symbols(f"v_{jj}")
+            v_args.append(v)
+    elif type(n_var) == list:
+        q_args = [diff_to_symb(var) for var in n_var]
+        v_args = [diff_to_symb(var.diff()) for var in n_var]
+    else:
+        raise TypeError(
+            "data type not undersood for q_vars, must be an integer or list of symbols"
+        )
+
+    for jj in range(max_n_var):
         u = symbols(f"u_{jj}")
         u_args.append(u)
-        args += [q, v, u]
+
     x_args = q_args + v_args
+    args = x_args + u_args
+
     for ii in range(len(expr_list)):
         expr = expr_list[ii]
         var_set = expr.atoms(Symbol)
@@ -406,12 +536,16 @@ def print_funcs(expr_list, n_var=0, flavour="numpy"):
                 if not symb in params:
                     params.append(symb)
             elif symb in u_args:
-                u_args_found.append(symb)
+                if not symb in u_args_found:
+                    u_args_found.append(symb)
             elif symb in x_args:
-                x_args_found.append(symb)
+                if not symb in x_args_found:
+                    x_args_found.append(symb)
         funcs.append(expr)
 
     params = sorted(params, key=get_str)
+    u_args_found = sorted(u_args_found, key=get_str)
+
     msg = "def F("
     if len(x_args_found) > 0:
         msg += "x, "
@@ -423,7 +557,7 @@ def print_funcs(expr_list, n_var=0, flavour="numpy"):
     if len(x_args_found) > 0:
         msg += f"    {x_args.__str__()[1:-1]} = unpack(x)\n"
     if len(u_args_found) > 0:
-        msg += f"    {u_args.__str__()[1:-1]} = unpack(u)\n"
+        msg += f"    {u_args_found.__str__()[1:-1]} = unpack(u)\n"
     if len(params) > 0:
         msg += f"    {params.__str__()[1:-1]} = params\n"
     if len(funcs) == 1:
