28796dc51898d901f1356eeb137895125dc52e99
Siro Moreno <siro.moreno.martin@gmail.com>
2021-11-11 18:04:55 +0100

    applied Black formatting

--- optibot/pseudospectral.py
+++ optibot/pseudospectral.py
@@ -12,26 +12,40 @@ from numpy import array, piecewise
 
 # --- Generating Collocation Points
 @lru_cache
-def LG(N, precission = 20):
-    return [ii.evalf(n = precission) for ii in legendre_poly(N, polys = True).real_roots()]
-@lru_cache
-def LGR(N, precission = 20):
-    pol = legendre_poly(N, polys = True) + legendre_poly(N-1, polys = True)
-    return [ii.evalf(n = precission) for ii in pol.real_roots()]
-@lru_cache
-def LGL(N, precission = 20):
-    root_list = [ii.evalf(n = precission) for ii in legendre_poly(N-1, polys = True).diff().real_roots()]
-    return [-1.,] + root_list + [1.,]
-@lru_cache
-def LGLm(N, precission = 20):
-    return LGL(N+2, precission)[1:-1]
-@lru_cache
-def LG2(N, precission = 20):
-    return [-1]+LG(N-2, precission)+[1]
+def LG(N, precission=20):
+    return [ii.evalf(n=precission) for ii in legendre_poly(N, polys=True).real_roots()]
+
 
 @lru_cache
-def coll_points(N, scheme, precission = 20):
-    '''
+def LGR(N, precission=20):
+    pol = legendre_poly(N, polys=True) + legendre_poly(N - 1, polys=True)
+    return [ii.evalf(n=precission) for ii in pol.real_roots()]
+
+
+@lru_cache
+def LGL(N, precission=20):
+    root_list = [
+        ii.evalf(n=precission)
+        for ii in legendre_poly(N - 1, polys=True).diff().real_roots()
+    ]
+    return (
+        [-1.0,] + root_list + [1.0,]
+    )
+
+
+@lru_cache
+def LGLm(N, precission=20):
+    return LGL(N + 2, precission)[1:-1]
+
+
+@lru_cache
+def LG2(N, precission=20):
+    return [-1] + LG(N - 2, precission) + [1]
+
+
+@lru_cache
+def coll_points(N, scheme, precission=20):
+    """
     Generates a list of len N with values of tau for collocation points
 
     Parameters
@@ -54,26 +68,28 @@ def coll_points(N, scheme, precission = 20):
     -------
     coll_points : list
         list of collocation points
-    '''
-    if scheme == 'LG':
+    """
+    if scheme == "LG":
         return LG(N, precission)
-    elif scheme == 'LGR':
+    elif scheme == "LGR":
         return LGR(N, precission)
-    elif scheme == 'LGR_inv':
+    elif scheme == "LGR_inv":
         return [-ii for ii in LGR(N, precission)[::-1]]
-    elif scheme in['LGL', 'D2'] :
+    elif scheme in ["LGL", "D2"]:
         return LGL(N, precission)
-    elif scheme == 'LGLm':
+    elif scheme == "LGLm":
         return LGLm(N, precission)
-    elif scheme == 'LG2':
+    elif scheme == "LG2":
         return LG(N, precission)
     else:
-        raise ValueError(f'Unsupported scheme {scheme}, valid schemes are: LG, LGR, LGR_inv, LGL, LGLm, LG2, D2')
-        
+        raise ValueError(
+            f"Unsupported scheme {scheme}, valid schemes are: LG, LGR, LGR_inv, LGL, LGLm, LG2, D2"
+        )
+
 
 @lru_cache
-def base_points(N, scheme, precission = 20):
-    '''
+def base_points(N, scheme, precission=20):
+    """
     Generates a list of len N with values of tau for lagrange base points
 
     Parameters
@@ -97,24 +113,27 @@ def base_points(N, scheme, precission = 20):
     -------
     base_points : list
         list of base points
-    '''
-    if scheme == 'LG':
-        return [-1.] + LG(N, precission)
-    elif scheme == 'LG_inv':
-        return LG(N, precission) + [1.]
-    elif scheme == 'LGR':
-        return LGR(N, precission) + [1.]
-    elif scheme == 'LGR_inv':
-        return [-1.] + [-ii for ii in LGR(N, precission)[::-1]]
-    elif scheme in['LGL', 'D2']:
+    """
+    if scheme == "LG":
+        return [-1.0] + LG(N, precission)
+    elif scheme == "LG_inv":
+        return LG(N, precission) + [1.0]
+    elif scheme == "LGR":
+        return LGR(N, precission) + [1.0]
+    elif scheme == "LGR_inv":
+        return [-1.0] + [-ii for ii in LGR(N, precission)[::-1]]
+    elif scheme in ["LGL", "D2"]:
         return LGL(N, precission)
-    elif scheme == 'LGLm':
-        return [-1.] + LGLm(N, precission) + [1.]
-    elif scheme == 'LG2':
+    elif scheme == "LGLm":
+        return [-1.0] + LGLm(N, precission) + [1.0]
+    elif scheme == "LG2":
         return LG2(N, precission)
     else:
-        raise ValueError(f'Unsupported scheme {scheme}, valid schemes are: LG, LG_inv LGR, LGR_inv, LGL, LGLm, LG2, D2')
-        
+        raise ValueError(
+            f"Unsupported scheme {scheme}, valid schemes are: LG, LG_inv LGR, LGR_inv, LGL, LGLm, LG2, D2"
+        )
+
+
 # --- Symbolic Lagrange Polynomials ---
 
 #### CREDIT: https://gist.github.com/folkertdev/084c53887c49a6248839 ####
@@ -122,8 +141,6 @@ def base_points(N, scheme, precission = 20):
 
 from operator import mul
 from functools import reduce, lru_cache
-
-
 
 
 # convenience functions
@@ -134,18 +151,20 @@ _product = lambda *args: reduce(mul, *(list(args) + [1]))
 # therefore, cache the result
 @lru_cache
 def lag_pol(labels, j):
-    x = symbols('x')
+    x = symbols("x")
+
     def gen(labels, j):
         current = labels[j]
         for m in labels:
             if m == current:
                 continue
             yield (x - m) / (current - m)
+
     return expand(_product(gen(labels, j)))
 
 
 def lagrangePolynomial(xs, ys):
-    '''
+    """
     Generates a symbolic polynomial that goes through all (x,y) given points
 
     Parameters
@@ -160,7 +179,7 @@ def lagrangePolynomial(xs, ys):
     total : SymPy symbolic polynomial
         DESCRIPTION.
 
-    '''
+    """
     # based on https://en.wikipedia.org/wiki/Lagrange_polynomial#Example_1
     total = 0
 
@@ -173,11 +192,12 @@ def lagrangePolynomial(xs, ys):
 
     return total
 
+
 # --- Barycentric Coordinates and Derivation Matrices ---
 
 
 def _v_sum(t_arr, i):
-    '''
+    """
     Generates the coefficient V for barycentric coordinates
 
     Parameters
@@ -192,19 +212,19 @@ def _v_sum(t_arr, i):
     v_i : float
         coefficient V.
 
-    '''
+    """
     n = len(t_arr)
     prod_coef = [ii for ii in range(n)]
     prod_coef.pop(i)
     v_i = 1.0
     for jj in prod_coef:
-        v_i *= (t_arr[i]-t_arr[jj])
-    return 1./v_i
+        v_i *= t_arr[i] - t_arr[jj]
+    return 1.0 / v_i
 
 
 @lru_cache
-def v_coef(N, i, scheme, precission = 20):
-    '''
+def v_coef(N, i, scheme, precission=20):
+    """
     Generates the coefficient V for barycentric coordinates
 
     Parameters
@@ -231,13 +251,14 @@ def v_coef(N, i, scheme, precission = 20):
     v_i : float
         coefficient V.
 
-    '''
+    """
     taus = base_points(N, scheme, precission)
     return _v_sum(taus, i)
 
+
 @lru_cache
-def matrix_D_bary(N, scheme, precission = 20):
-    '''
+def matrix_D_bary(N, scheme, precission=20):
+    """
     Generates the Derivation Matrix for the given scheme from
     barycentric coordinates
 
@@ -263,7 +284,7 @@ def matrix_D_bary(N, scheme, precission = 20):
     v_i : float
         coefficient V.
 
-    '''
+    """
     taus = base_points(N, scheme, precission)
     M = zeros(N)
     v_arr = [v_coef(N, ii, scheme, precission) for ii in range(N)]
@@ -271,14 +292,14 @@ def matrix_D_bary(N, scheme, precission = 20):
         j_range = [j for j in range(N)]
         j_range.pop(i)
         for j in j_range:
-            M[i, j] = (v_arr[j]/v_arr[i])/(taus[i]-taus[j])
+            M[i, j] = (v_arr[j] / v_arr[i]) / (taus[i] - taus[j])
     for j in range(N):
-        M[j,j] = -sum(M[j,:])
+        M[j, j] = -sum(M[j, :])
     return M
 
 
 def bary_poly(t_arr, y_arr):
-    '''
+    """
     Generates a numeric function of t that corresponds to the polynomial
     that passes through the points (t, x) using the barycentric formula
 
@@ -294,23 +315,23 @@ def bary_poly(t_arr, y_arr):
     polynomial : Function F(t)
         polynomial numerical function
 
-    '''
-    t = symbols('t')
+    """
+    t = symbols("t")
     n = len(t_arr)
     v_arr = [_v_sum(t_arr, ii) for ii in range(n)]
     sup = 0
     for i in range(n):
-        sup+= v_arr[i]*y_arr[i]/(t-t_arr[i])
+        sup += v_arr[i] * y_arr[i] / (t - t_arr[i])
     inf = 0
     for i in range(n):
-        inf+= v_arr[i]/(t-t_arr[i])
-    poly_fun = lambdify([t,], sup/inf)
-    
+        inf += v_arr[i] / (t - t_arr[i])
+    poly_fun = lambdify([t,], sup / inf)
+
     def new_poly(t):
-        t = array(t, dtype='float64')
+        t = array(t, dtype="float64")
         cond_list = [t == t_i for t_i in t_arr]
         func_list = list(y_arr)
         func_list.append(poly_fun)
         return piecewise(t, cond_list, func_list)
-        
+
     return new_poly
