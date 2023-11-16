7630c6f5b7fa0f731c2a26c927f0e5a4e8176aad
Siro Moreno <siro.moreno.martin@gmail.com>
2023-11-16 19:03:28 +0100

    bu-pseudospectral dynamic errors

--- chords/bu_pseudospectral.py
+++ chords/bu_pseudospectral.py
@@ -21,13 +21,16 @@ from .pseudospectral import (
     bary_poly,
     bary_poly_2d,
     extend_u_array,
+    find_der_polyline,
 )
-from .util import gauss_rep_integral, poly_integral, poly_integral_2d
-from .piecewise import interp_2d, is2d
+from .util import gauss_rep_integral, poly_integral_2d
+from .piecewise import interp_2d, is2d, get_x_divisions
 from functools import lru_cache
-from numpy import zeros, array, concatenate, interp, gradient, zeros_like, linspace
-from sympy import jacobi_poly
+from numpy import zeros, array, concatenate, interp, gradient, linspace
+
+# from sympy import jacobi_poly
 from math import factorial
+from copy import copy
 
 
 _implemented_schemes = ["LG", "LGL", "LGR", "LGR_inv", "JG", "JGR", "JGR_inv", "JGL"]
@@ -435,7 +438,6 @@ def Polynomial_interpolations_BU(xx_dot, x_0, uu, scheme, scheme_order, t0, tf, 
 
     coll_points = tau_to_t_points(BU_coll_points(n_col, scheme, scheme_order), t0, tf)
 
-    u_poly = bary_poly_2d(coll_points, uu)
     highest_der_poly = bary_poly_2d(coll_points, highest_der_col)
 
     q_and_der_polys = [
@@ -448,7 +450,11 @@ def Polynomial_interpolations_BU(xx_dot, x_0, uu, scheme, scheme_order, t0, tf, 
         )
         q_and_der_polys.append(_new_poly)
 
-    return u_poly, q_and_der_polys[::-1]
+    if uu is None:
+        return q_and_der_polys[::-1]
+    else:
+        u_poly = bary_poly_2d(coll_points, uu)
+        return u_poly, q_and_der_polys[::-1]
 
 
 def interpolations_BU_pseudospectral(
@@ -546,17 +552,11 @@ def interpolations_BU_pseudospectral(
         u_pol, q_and_der_polys = Polynomial_interpolations_BU(
             xx_dot, x_0, uu, scheme, scheme_order, t0, tf, n_col
         )
-        if u_interp == "pol":
-            u_arr = u_pol(t_arr)
-        if x_interp == "pol":
-            q_and_der_arrs = []
-            for ii in range(scheme_order + 1):
-                q_and_der_arrs.append(q_and_der_polys[ii](t_arr))
-            q_and_der_arrs = concatenate(tuple(q_and_der_arrs), axis=1)
-            x_arr = q_and_der_arrs[:, :-n_q]
-            x_dot_arr = q_and_der_arrs[:, n_q:]
 
-    if u_interp == "lin":
+    if u_interp == "pol":
+        u_arr = u_pol(t_arr)
+
+    elif u_interp == "lin":
         tau_u, uu = extend_u_array(uu, scheme, n_col, scheme_order)
         t_u = tau_to_t_points(tau_u, t0, tf)
 
@@ -575,10 +575,16 @@ def interpolations_BU_pseudospectral(
         u_arr = hermite(t_u, uu, uu_dot)(t_arr)
     else:
         raise NameError(
-            'Invalid interpolation method for u.\n valid options are "pol", "lin", "smooth"'
+            f'Invalid interpolation method for u:{u_interp}.\n valid options are "pol", "lin", "smooth"'
         )
-
-    if x_interp == "lin":
+    if x_interp == "pol":
+        q_and_der_arrs = []
+        for ii in range(scheme_order + 1):
+            q_and_der_arrs.append(q_and_der_polys[ii](t_arr))
+        q_and_der_arrs = concatenate(tuple(q_and_der_arrs), axis=1)
+        x_arr = q_and_der_arrs[:, :-n_q]
+        x_dot_arr = q_and_der_arrs[:, n_q:]
+    elif x_interp == "lin":
         x_arr = interp_2d(t_arr, t_x, xx)
         x_dot_arr = interp_2d(t_arr, t_x, xx_dot)
 
@@ -588,6 +594,356 @@ def interpolations_BU_pseudospectral(
         x_dot_arr = herm.derivative()(t_arr)
     else:
         raise NameError(
-            'Invalid interpolation method for x.\n valid options are "pol", "lin", "Hermite"'
+            f'Invalid interpolation method for x:{x_interp}.\n valid options are "pol", "lin", "Hermite"'
         )
     return x_arr, x_dot_arr, u_arr
+
+
+def _v_sum(t_arr, i):
+    """
+    Generates the coefficient V for barycentric coordinates
+
+    Parameters
+    ----------
+    t_arr : iterable of floats
+        values of t
+    i : int
+        index of current point.
+
+    Returns
+    -------
+    v_i : float
+        coefficient V.
+
+    """
+    n = len(t_arr)
+    prod_coef = [ii for ii in range(n)]
+    prod_coef.pop(i)
+    v_i = 1.0
+    for jj in prod_coef:
+        v_i *= t_arr[i] - t_arr[jj]
+    return 1.0 / v_i
+
+
+def _matrix_D_bary(t_arr):
+    """
+    Generates the Derivation Matrix for the given scheme from
+    barycentric coordinates
+
+    Parameters
+    ----------
+    t_arr: array of times
+
+    Returns
+    -------
+    M : NumPy Array
+        Derivation Matrix.
+
+    """
+    N = len(t_arr)
+    M = zeros((N, N), dtype=float)
+    v_arr = [_v_sum(t_arr, ii) for ii in range(N)]
+    for i in range(N):
+        j_range = [j for j in range(N)]
+        j_range.pop(i)
+        for j in j_range:
+            M[i, j] = (v_arr[j] / v_arr[i]) / (t_arr[i] - t_arr[j])
+    for j in range(N):
+        M[j, j] = -sum(M[j, :])
+    return M
+
+
+def interpolations_deriv_BU_pseudospectral(
+    xx,
+    xx_dot,
+    scheme,
+    scheme_order,
+    deriv_order,
+    t0,
+    tf,
+    x_interp="pol",
+    n_interp=5000,
+):
+    """
+    Generates arrays of equispaced points with values of interpolations of the
+    derivatives of x.
+
+    'x_interp' define the way in which we interpolate the values
+    of q, v and u between the given points.
+
+    Parameters
+    ----------
+    xx : Numpy Array
+        Values known of x(t)
+    xx_dot : Numpy Array
+        Values known of x_dot(t)
+    scheme : str
+        Pseudospectral scheme used in the optimization.
+        Acceptable values are:
+            'LG'
+            'LGR'
+            'LGR_inv'
+            'LGL'
+            'JG'
+            'JGR'
+            'JGR_inv'
+            'JGL'
+    scheme_order : int
+        differential order of the problem
+    deriv_order : int
+        differential order of the interpolations
+    t0 : float
+        starting time of interval of analysis
+    t1 : float
+        ending time of interval of analysis
+    u_interp :  string, optional
+        Model of the interpolation that must be used. The default is "pol".
+        Acceptable values are:
+            "pol": corresponding polynomial interpolation
+            "lin": lineal interpolation
+            "smooth": 3d order spline interpolation
+    x_interp : string, optional
+        Model of the interpolation that must be used. The default is "pol".
+        Acceptable values are:
+            "pol": corresponding polynomial interpolation
+            "lin": lineal interpolation
+            "Hermite": Hermite's 3d order spline interpolation
+    n_interp : int, default 5000
+        number of interpolation points
+
+    Raises
+    ------
+    NameError
+        When an unsupported value for scheme, x_interp or u_interp is used.
+
+    Returns
+    -------
+    x_arr, x_dot_arr, u_arr : Numpy array
+        equispaced values of interpolations.
+    """
+    if scheme[:3] == "BU_":
+        scheme = scheme[3:]
+    if is2d(xx):
+        x_0 = xx[0, :]
+    else:
+        x_0 = xx[0]
+    n_q = len(x_0) // scheme_order
+    highest_der = xx_dot[:, -n_q:]
+    coll_index = get_coll_indices(scheme)
+    highest_der_col = highest_der[coll_index, :]
+    n_col = highest_der_col.shape[0]
+
+    if x_interp == "Hermite":
+        from scipy.interpolate import CubicHermiteSpline as hermite
+
+    if scheme not in _implemented_schemes:
+        NameError(f"Invalid scheme.\n valid options are {_implemented_schemes}")
+
+    t_arr = linspace(t0, tf, n_interp)
+    t_x = tau_to_t_points(
+        array([-1.0] + BU_construction_points(n_col, scheme, scheme_order)), t0, tf
+    )
+
+    if x_interp == "pol":
+        D = _matrix_D_bary(t_x)
+        q_and_der_polys = Polynomial_interpolations_BU(
+            xx_dot, x_0, None, scheme, scheme_order, t0, tf, n_col
+        )
+        for jj in range(deriv_order - 1):
+            highest_der = D @ highest_der
+            highest_der_poly = bary_poly_2d(t_x, highest_der)
+            q_and_der_polys.append(highest_der_poly)
+
+        q_and_der_arrs = []
+        for ii in range(scheme_order):
+            q_and_der_arrs.append(q_and_der_polys[ii + deriv_order](t_arr))
+        x_deriv_arr = concatenate(tuple(q_and_der_arrs), axis=1)
+
+    elif x_interp == "lin":
+        if deriv_order == 0:
+            x_deriv_arr = interp_2d(t_arr, t_x, xx)
+        elif deriv_order == 1:
+            x_deriv_arr = find_der_polyline(t_arr, t_x, xx)
+        else:
+            x_deriv_arr = zeros((len(t_arr), len(x_0)), dtype=float)
+
+    elif x_interp == "Hermite":
+        herm = hermite(t_x, xx, xx_dot)
+        for ii in range(deriv_order):
+            herm = herm.derivative()
+        x_deriv_arr = herm(t_arr)
+    else:
+        raise NameError(
+            'Invalid interpolation method for x.\n valid options are "pol", "lin", "Hermite"'
+        )
+    return x_deriv_arr
+
+
+def dynamic_error_BU(
+    x_arr,
+    x_dot_arr,
+    u_arr,
+    params,
+    scheme,
+    tf,
+    F,
+    t0=0,
+    scheme_order=2,
+    u_interp="pol",
+    x_interp="pol",
+    n_interp=2000,
+    scheme_params=None,
+    mode="q",
+):
+    """
+    Generate arrays of equispaced points with values of dynamic error.
+
+    If x(t) = [q(t), v(t)], and the physics equation states that x' = F(x, u),
+    which is equivalent to [q', v'] = [v , G(q, v, u)] we can define the
+    dynamic errors at a point t as:
+        dyn_q_err = q'(t) - v(t)
+        dyn_v_err = v'(t) - G(q(t), v(t), u(t))
+        dyn_2_err_a = q''(t) - G(q(t), v(t), u(t))
+        dyn_2_err_b = q''(t) - G(q(t), q'(t), u(t))
+
+    'scheme' and 'u_scheme' define the way in which we interpolate the values
+    of q, v and u between the given points.
+
+
+    Parameters
+    ----------
+    x_arr : Numpy Array
+        Values known of x(t)
+    x_dot_arr : Numpy Array
+        Values known of x'(t)
+    u_arr : Numpy Array
+        Values known of u(t)
+    params : list
+        Physical problem parameters to be passed to F
+    scheme : str
+        Pseudospectral scheme used in the optimization.
+        Acceptable values are:
+            'LG'
+            'LGR'
+            'LGR_inv'
+            'LGL'
+            'JG'
+            'JGR'
+            'JGR_inv'
+            'JGL'
+    t1 : float
+        ending time of interval of analysis
+    F : Function of (x, u, params)
+        A function of a dynamic sistem, so that
+            x' = F(x, u, params)
+    t0 : float, default = 0
+        starting time of interval of analysis
+    scheme_order : int, default 2
+        differential order of the problem
+    u_interp :  string, optional
+        Model of the interpolation that must be used. The default is "pol".
+        Acceptable values are:
+            "pol": corresponding polynomial interpolation
+            "lin": lineal interpolation
+            "smooth": 3d order spline interpolation
+    x_interp : string, optional
+        Model of the interpolation that must be used. The default is "pol".
+        Acceptable values are:
+            "pol": corresponding polynomial interpolation
+            "lin": lineal interpolation
+            "Hermite": Hermite's 3d order spline interpolation
+    n_interp : int, default 2000
+        number of interpolation points
+    scheme_params :dict or none, optional
+        Aditional parameters of the scheme. The default is None.
+    mode : str, 'q' or 'x', default 'q'.
+        if 'q': q and its derivatives will be used in G, such as:
+            G(q(t), q'(t), u(t))
+        if 'x': components of x will be used in G, such as:
+            G(q(t), v(t), u(t))
+
+    Returns
+    -------
+    dyn_errs: list of lists of arrs that contain the dynamic errors
+        A list of [order] items, the n-th item is a list of the n-th dynamic errors.
+        First item is the list (first order errors):
+            [q' - v,
+             v' - a,
+             ...
+             x'[last] - f,]
+        last item in the list (highest order errors):
+            [q^(order) - f,]
+
+    """
+    if scheme[:3] == "BU_":
+        scheme = scheme[3:]
+    if is2d(x_arr):
+        x_0 = x_arr[0, :]
+    else:
+        x_0 = x_arr[0]
+    n_q = len(x_0) // scheme_order
+
+    if scheme_params is None:
+        scheme_params = {}
+    x_and_derivs = []
+    scheme_params["order"] = scheme_order
+
+    x_interp_arr, x_dot_interp_arr, u_interp_arr = interpolations_BU_pseudospectral(
+        x_arr,
+        x_dot_arr,
+        u_arr,
+        scheme,
+        scheme_order,
+        t0,
+        tf,
+        u_interp=u_interp,
+        x_interp=x_interp,
+        n_interp=n_interp,
+    )
+
+    x_and_derivs.append(get_x_divisions(x_interp_arr, scheme_order))
+    for jj in range(1, scheme_order + 1):
+        x_and_derivs.append(
+            get_x_divisions(
+                interpolations_deriv_BU_pseudospectral(
+                    x_arr,
+                    x_dot_arr,
+                    scheme,
+                    scheme_order,
+                    jj,
+                    t0,
+                    tf,
+                    x_interp=x_interp,
+                    n_interp=n_interp,
+                ),
+                scheme_order,
+            )
+        )
+
+    q_and_d_interp = copy(x_interp_arr)
+    for jj in range(scheme_order):
+        q_and_d_interp[:, n_q * jj : n_q * (jj + 1)] = x_and_derivs[jj][0]
+
+    if mode == "q":
+        x_in_f = q_and_d_interp
+    elif mode == "x":
+        x_in_f = x_interp
+    else:
+        raise ValueError(
+            f"Value of mode {mode} not valid. Valid values are 'q' and 'x'."
+        )
+
+    f_interp = zeros([n_interp, n_q])
+    for ii in range(n_interp):
+        f_interp[ii, :] = F(x_in_f[ii], u_interp_arr[ii], params)[-n_q:]
+    x_and_derivs[0].append(f_interp)
+
+    dyn_errs = []
+    for jj in range(scheme_order):
+        dyn_errs_order = []
+        for ii in range(scheme_order - jj):
+            dyn_errs_order.append(
+                x_and_derivs[jj + 1][ii] - x_and_derivs[0][ii + jj + 1]
+            )
+        dyn_errs.append(dyn_errs_order)
+    return dyn_errs
